f7662b715ca7d0276cd03e1cfb304d7d22a125c9
Kristian Perez <kpere192@fiu.edu>
2024-04-17 14:15:51 -0400

    Added clean_file method to remove any words under 5 letters. Also added select random word from file

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,8 +1,19 @@
 file = "1000 words.txt"
 
+def clean_file(file)
+  lines = File.readlines(file)
+  cleaned_file = lines.map { |line| line.length < 5? "" : line}
+  cleaned_file.reject! { |line| line.empty? }
+  File.open(file, "w") { |line| line.puts cleaned_file}
+end
+
+def select_word(file)
+  lines = File.readlines(file)
+  word = lines[rand(0..lines.length-1)]
+end
+
 if File.exist?(file)
-    lines = File.readlines(file)
-    lines.each do |line|
-      puts line
-    end
+  clean_file(file)
+  word = select_word(file)
+  puts word
 end
