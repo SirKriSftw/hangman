953bb277e0195ff61d4509961398eb666f809e06
Kristian Perez <kpere192@fiu.edu>
2024-04-17 14:39:34 -0400

    print board method now takes in acct guessed letters and displays them (set to all values for testing, typically empty)

--- lib/game.rb
+++ lib/game.rb
@@ -5,14 +5,21 @@ class Game
 
   def initialize(word)
     @word = word
-    @guessed_letters = []
+    @guessed_letters = ["a","e","i","o","u"]
     @player = Player.new(self)
     print_board()
   end
 
   def print_board()
     correct_letters = word.split("")
+    # Removes \n at the end of line
     correct_letters.pop
-    correct_letters.each {|e| print "_ "}
+    correct_letters.each do |letter|
+      if(guessed_letters.include?(letter))
+        print "#{letter} "
+      else
+      print "_ "
+      end
+    end
   end
 end
