ae112402b9f94e3126ae2d3da6d07f8ec93536ba
Kristian Perez <kpere192@fiu.edu>
2024-04-18 14:54:08 -0400

    Ask player if they want to load a game. If they do display saved game files

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -3,6 +3,17 @@ require_relative "player"
 MIN_LENGTH = 5
 MAX_LENGTH = 12
 
+print "Would you like to load a game? (Y/N): "
+input = gets.chomp
+
+unless input.upcase == "Y"
+  play_new()
+else
+  play_load()
+end
+
+
+
 def clean_file(file)
   lines = File.readlines(file)
   cleaned_file = lines.map { |line| line.length < MIN_LENGTH || line.length > MAX_LENGTH ? "" : line}
@@ -15,11 +26,20 @@ def select_word(file)
   word = lines[rand(0..lines.length-1)].strip
 end
 
-file = "10000 words.txt"
-if File.exist?(file)
-  clean_file(file)
-  word = select_word(file)
+def play_new()
+  file = "10000 words.txt"
+  if File.exist?(file)
+    clean_file(file)
+    word = select_word(file)
 
-  game = Game.new(word);
-  game.play()
+    game = Game.new(word);
+    game.play()
+  end
+end
+
+def play_load()
+  load_dir = Dir.new("lib/saved_games")
+  saved_games = load_dir.children[1..(load_dir.children.length-1)]
+  saved_games.each_with_index {|game, i| puts "(#{i+1}) #{game}" }
+  print "What game would you like to load?(1/2/3): "
 end
